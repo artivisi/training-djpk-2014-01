d1a853d0a45018abc731570c62c39477dfce7abf
Endy Muhardin <endy.muhardin@gmail.com>
2014-01-28 10:41:21 +0700

    sample file

---
+++ src/main/java/id/go/kemenkeu/djpk/usermanagement/App.java
@@ -0,0 +1,13 @@
+package id.go.kemenkeu.djpk.usermanagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
