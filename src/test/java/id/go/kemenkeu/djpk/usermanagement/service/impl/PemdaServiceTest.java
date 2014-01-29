1ec355fea635955a9a1f1b4f9731faee46e0b5d8
Endy Muhardin <endy.muhardin@gmail.com>
2014-01-29 14:32:15 +0700

    reset isi database sebelum tes

--- src/test/java/id/go/kemenkeu/djpk/usermanagement/service/impl/PemdaServiceTest.java
+++ src/test/java/id/go/kemenkeu/djpk/usermanagement/service/impl/PemdaServiceTest.java
@@ -8,6 +8,11 @@ package id.go.kemenkeu.djpk.usermanagement.service.impl;
 
 import id.go.kemenkeu.djpk.usermanagement.domain.Pemda;
 import id.go.kemenkeu.djpk.usermanagement.service.UserManagementService;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -30,14 +35,20 @@ public class PemdaServiceTest {
     @Autowired
     private UserManagementService ums;
     
+    @Autowired private DataSource dataSource;
+    
     @BeforeClass
     public static void inisialisasi(){
         System.out.println("Menjalankan inisialisasi");
     }
     
     @Before
-    public void sebelumTest(){
-        System.out.println("Sebelum Test");
+    public void sebelumTest() throws Exception {
+        System.out.println("Kosongkan tabel pemda");
+        String sql = "delete from tpemda";
+        Connection conn = dataSource.getConnection();
+        conn.createStatement().executeUpdate(sql);
+        conn.close();
     }
     
     @After
@@ -67,7 +78,42 @@ public class PemdaServiceTest {
     }
     
     @Test
-    public void testKedua(){
-        System.out.println("Test Kedua");
+    public void testInsertBanyak() throws Exception {
+        //Simpan banyak
+        List<Object> daftarPemda = new ArrayList<Object>();
+        Pemda p = new Pemda();
+        p.setKode("P-001");
+        p.setKodeSatuanKerja("ABC0");
+        p.setUraian("Pemda Tester");
+        p.setUraianSingkat("Tester");
+        daftarPemda.add(p);
+        
+        Pemda p1 = new Pemda();
+        p1.setKode("P-002");
+        p1.setKodeSatuanKerja("ABC1");
+        p1.setUraian("Pemda Tester");
+        p1.setUraianSingkat("Tester");
+        daftarPemda.add(p1);
+        
+        Pemda p2 = new Pemda();
+        p2.setKode("P-003");
+        p2.setKodeSatuanKerja("ABC2");
+        p2.setUraian("Pemda Tester");
+        p2.setUraianSingkat("Tester");
+        daftarPemda.add(p2);
+        
+        ums.simpanBanyak(daftarPemda);
+        
+        String sqlCek = "select count(*) from tpemda";
+        Connection conn = dataSource.getConnection();
+        
+        ResultSet rs = conn.createStatement().executeQuery(sqlCek);
+        Assert.assertTrue(rs.next());
+        
+        Long count = rs.getLong(1);
+        Assert.assertEquals(new Long(3), count);
+        
+        rs.close();
+        conn.close();
     }
 }
